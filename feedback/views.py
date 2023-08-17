74a947acb3eaf2fc362086b4015559c467ac83fa
Mani <manideep4536@gmail.com>
2023-08-17 22:11:16 +0530

    update

--- feedback/views.py
+++ feedback/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from feedback.forms import feedbackform
 from feedback.models import feedbackmodel
+from django.http import HttpResponse
 # Create your views here.
 def createfeed(request):
     form=feedbackform()
+    if request.method=="POST":
+        form=feedbackform(request.POST)
+        form.save()
+        return HttpResponse("data is stored")
     return render(request,"home.html",{"form":form})
+
+def readfeed(request):
+    res=feedbackmodel.objects.all()
+    return render(request,'details.html',{'res':res})
