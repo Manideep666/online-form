f7b0b217214615147bdd8560a0aff594628cab2b
Mani <manideep4536@gmail.com>
2023-08-17 21:29:00 +0530

    first commit

--- feedback/views.py
+++ feedback/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from feedback.forms import feedbackform
+from feedback.models import feedbackmodel
 # Create your views here.
+def createfeed(request):
+    form=feedbackform()
+    return render(request,"home.html",{"form":form})
